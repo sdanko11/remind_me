027931c08be02d779e27fe796ac3ec4285ed183c
steve <stevedanko1@gmail.com>
2014-03-14 15:33:49 -0400

    initial commit set up postgres db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ReminderApp::Application.config.secret_key_base = 'a7df6da38ea44b72341e1021a5170426c6f10ad4196cc092ace18609a7154b9d56f3b2d6ca1bd7af7c79a560c6b98f685904710144a1afb2e906ab6b55f7843f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ReminderApp::Application.config.session_store :cookie_store, key: '_reminder_app_session'
